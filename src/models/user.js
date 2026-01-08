f42e7ffa2df2aed93ffed7822e515ca1745100aa
Akani Shibiri <147254957+Re-EL123@users.noreply.github.com>
2026-01-08 00:32:22 -0900

    Add children array reference to User model

--- src/models/user.js
+++ src/models/user.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     surname: { type: String, required: true },
+    
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { 
@@ -15,7 +16,13 @@ const userSchema = new mongoose.Schema(
     address: { type: String },
     location: { type: String },
     latitude: { type: Number },
-    longitude: { type: Number },
+    longitude: { type: Number },,
+    children: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Child'
+      }
+    ]
   },
   { timestamps: true }
 );
