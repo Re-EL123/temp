3d37c3564ec5767a58c8c489082fa9cecb23a3eb
Akani Shibiri <147254957+Re-EL123@users.noreply.github.com>
2026-01-15 05:44:44 -0900

    Update user.js

--- src/models/user.js
+++ src/models/user.js
@@ -2,22 +2,39 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    surname: { type: String, required: true },
+    name: { type: String, required: true, trim: true },
+    surname: { type: String, required: true, trim: true },
     
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true , trim: true},
+    password: { type: String, required: true , trim: true},
     role: { 
       type: String, 
       enum: ["user", "parent", "driver", "admin"], 
       default: "user" 
     },
     onboardingCompleted: { type: Boolean, default: false },
-    phone: { type: String },
-    address: { type: String },
+    phone: { type: String , trim: true},
+    address: { type: String , trim: true},
     location: { type: String },
     latitude: { type: Number },
     longitude: { type: Number },
+    registrationNumber: { 
+                        type: String, trim: true,
+                        required: function() { return this.role === 'driver'; }
+                },
+    passengerSeats: { 
+                        type: Number, trim: true,
+                        required: function() { return this.role === 'driver'; }
+                },
+    carBrand:  { 
+                        type: String, trim: true,
+                        required: function() { return this.role === 'driver'; }
+                },
+    carModel:  { 
+                        type: String, trim: true,
+                        required: function() { return this.role === 'driver'; }
+                },
+    
     children: [
       {
         type: mongoose.Schema.Types.ObjectId,
@@ -25,6 +42,8 @@ const userSchema = new mongoose.Schema(
       }
     ]
   },
+  
+
   { timestamps: true }
 );
 
