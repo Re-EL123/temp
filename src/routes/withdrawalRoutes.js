57e30f11152755e407e14624483231d5653aa04c
Akani Shibiri <147254957+Re-EL123@users.noreply.github.com>
2026-04-02 15:02:43 +0200

    Update withdrawal controller import to walletController

--- src/routes/withdrawalRoutes.js
+++ src/routes/withdrawalRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const verifyToken = require("../middleware/authMiddleware");
-const withdrawalController = require("../controllers/withdrawalController");
+const withdrawalController = require("../controllers/walletController");
 
 // Create a withdrawal request
 router.post("/", verifyToken(), withdrawalController.requestWithdrawal);
